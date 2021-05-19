80ddfb9a1ac6c6794c81300330cf9412ee8495f9
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-05-20 00:48:26 +0500

    for loop

---
+++ app.js
@@ -0,0 +1,68 @@
+//*********** Task : 01************************/
+
+// for(var i=1; i <= 7; i++)
+// {
+
+//  for(var j=7; j>=i; j--)
+// {
+//   document.write('*');
+//  }
+//   document.write('<br/>');
+// }
+
+
+//*********** Task : 02 a ************************/
+
+// for(var i = 1; i <= 4; i++){
+//     document.write('*****' + '<br>')
+// }
+
+//*********** Task : 02 b ************************/
+
+// for(var i=1; i <= 5; i++)
+// {
+
+//  for(var j=1; j<=i; j++)
+// {
+//   document.write('*');
+//  }
+//   document.write('<br>')
+// }
+
+
+//*********** Task : 02 c ************************/
+
+// for(var i=1; i <= 5; i++)
+// {
+
+//  for(var j=5; j>=i; j--)
+// {
+//   document.write('*');
+//  }
+//   document.write('<br/>');
+// }
+
+
+
+//*********** Task : 02 d ************************/
+
+// for(var i=1; i <= 20; i++)
+// {
+
+//  for(var j=1; j<=i; j++)
+//  {
+//     document.write('*');
+
+//     }
+//     document.write('<br>')
+//  }
+
+// for(var k=1; k <= j; k++)
+// {
+
+//  for(var l=j; l>=k; l--)
+// {
+//   document.write('*');
+//  }
+//   document.write('<br/>');
+// }
